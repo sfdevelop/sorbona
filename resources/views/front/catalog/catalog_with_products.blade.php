d4d427e91ac3538f2d1879f0c224de996c188f8a
fox <sfdevelop747@gmail.com>
2024-11-18 01:39:32 +0200

    feat(manufFilter): front
    Signed-off-by: fox <sfdevelop747@gmail.com>

--- resources/views/front/catalog/catalog_with_products.blade.php
+++ resources/views/front/catalog/catalog_with_products.blade.php
@@ -4,10 +4,10 @@
     <div class="breadcrumb__container">
         <ul class="breadcrumb">
             <li><a href="{{route('home')}}">{{__('front.menu.main')}}</a></li>
-            <span>/</span>
-            <li><a href="subcategory.html">Запорно - регулирующая арматура</a></li>
-            <span>/</span>
-            <li><a href="subcategory2.html">Шаровые краны</a></li>
+            @if ($category->parentCategory)
+                <span>/</span>
+                <li><a href="{{route('category', $category->parentCategory->slug)}}">{{$category->parentCategory->title}}</a></li>
+            @endif
             <span>/</span>
             <li>{{$category->title}}</li>
         </ul>
@@ -18,8 +18,8 @@
         <div class="catalog__container section__container_fullmob">
             <h1 class="catalog__title">{{$category->title}}</h1>
             <div class="category">
-                    @include('front.catalog._filters_in_category')
-                    @include('front.catalog._products_in_category')
+                @include('front.catalog._filters_in_category')
+                @include('front.catalog._products_in_category')
             </div>
         </div>
     </section>
