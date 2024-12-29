a0f64beba5537881fbd66922543236521a01364b
fox <sfdevelop747@gmail.com>
2024-12-29 20:45:42 +0200

    fix(registration): front
    Signed-off-by: fox <sfdevelop747@gmail.com>

--- resources/views/layout/front/menu.blade.php
+++ resources/views/layout/front/menu.blade.php
@@ -126,12 +126,28 @@
             </div>
 
             <div class="header__line"></div>
-            <a href="#authorization" data-fancybox class="header__login">
-                <div class="header__login-icon">
-                    <svg><use xlink:href="{{asset('front/img/icons/icons.svg#icon-login')}}"></use></svg>
-                </div>
-                <div class="header__login-text">{{__('front.sign_in')}}</div>
-            </a>
+
+            @guest
+                <a href="{{route('login')}}"
+                   class="header__login"
+                >
+                    <div class="header__login-icon">
+                        <svg><use xlink:href="{{asset('front/img/icons/icons.svg#icon-login')}}"></use></svg>
+                    </div>
+                    <div class="header__login-text">{{__('front.sign_in')}}</div>
+                </a>
+            @endguest
+
+            @auth
+                <a href="#" class="header__login autorized">
+                    <div class="header__login-icon">
+                        <svg><use xlink:href="{{asset('front/img/icons/icons.svg#icon-login')}}"></use></svg>
+                    </div>
+                    <div class="header__login-text">{{auth()->user()->name ?? ''}}</div>
+                </a>
+            @endauth
+
+
 
             <span class="header__line"></span>
             <div class="header__basket">
