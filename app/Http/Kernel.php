ae29a95f1d17d6f9c24e59f5349c3a136de9c0ce
sunnyday80 <denis.cherkasov@gmail.com>
2025-01-27 11:00:36 +0200

    Last url middleware change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLanguage::class,
-            \App\Http\Middleware\StoreCurrentUrl::class,
         ],
 
         'api' => [
