cfc681408373ecef545ce8f6a5e33cc7395f3c47
fox <sfdevelop@gmail.com>
2025-05-28 14:50:03 +0300

    refactor(validation): admin
    validation

--- app/Http/Requests/Livewier/CreateOrderRequest.php
+++ app/Http/Requests/Livewier/CreateOrderRequest.php
@@ -6,9 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateOrderRequest extends FormRequest
 {
+    protected string $delivery;
+    protected string $payment;
+
+    public function setDeliveryAndPayment($delivery, $payment): static
+    {
+        $this->delivery = $delivery;
+        $this->payment = $payment;
+        return $this;
+    }
+
     public function rules(): array
     {
-        return [
+        $rules = [
             'name' => 'required|string|min:3',
             'surname' => 'required|string|min:3',
             'phone' => 'required|string',
@@ -18,6 +28,32 @@ class CreateOrderRequest extends FormRequest
             'comment' => 'nullable|string',
             'total' => 'required|string',
         ];
+
+        // Додаємо правила валідації в залежності від вибраного методу доставки
+        if ($this->delivery == 'deliveryMethodUp') {
+            $rules = array_merge($rules, [
+                'region' => 'required|string|min:3',
+                'locality' => 'required|string|min:3',
+                'index' => 'required|string',
+            ]);
+        }
+
+        if ($this->delivery == 'deliveryMethodNp') {
+            $rules = array_merge($rules, [
+                'selectedNpCity' => 'required|string',
+                'selectedNpDepot' => 'required|string',
+            ]);
+        }
+
+        if ($this->payment == 'paymentMethodBank') {
+            $rules = array_merge($rules, [
+                'companyName' => 'required|string|min:3',
+                'inn' => 'required|string|min:3',
+                'edrpou' => 'required|string',
+            ]);
+        }
+
+        return $rules;
     }
 
     public function authorize(): bool
